962e4efde7687b93da6e7d9ab7cad3a6d1b32d18
b3hr4d <behradmusiclove@yahoo.com>
2024-09-11 11:17:39 +0300

    Refactor fetch function to add CORS headers

--- src/index.ts
+++ src/index.ts
@@ -62,6 +62,8 @@ export default {
 				headers: {
 					'Content-Type': 'application/json',
 					'Access-Control-Allow-Origin': '*',
+					'Access-Control-Allow-Methods': 'POST, OPTIONS',
+					'Access-Control-Allow-Headers': 'Content-Type, Authorization, Idempotency-Key',
 				},
 			});
 		} catch (error: any) {
